ae8aff12f0856eddb7b92b73533191c630235705
yanad <yana.danilova1509@gmail.com>
2022-08-18 16:33:55 +0200

    homework2

---
+++ lesson2-jspring/src/main/java/de/danilova/AppConfig.java
@@ -0,0 +1,9 @@
+package de.danilova;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("de.danilova")
+public class AppConfig {
+}
---
+++ lesson2-jspring/src/main/java/de/danilova/ProductService.java
@@ -0,0 +1,17 @@
+package de.danilova;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class ProductService {
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Optional<Product> findById(int id) {
+        return productRepository. findProductById(id);
+    }
+}
